30faa168dc42b8a486f16f21d9267253c76cd5cd
jannatgull <jannatgullpopalxee@gmail.com>
2025-08-19 20:26:39 +0500

    Ques.10
    Salary calculator

---
+++ Question10.py
@@ -0,0 +1,6 @@
+basicsalary = int(input("Enter salary:"))
+HRA = (20 / 100) * basicsalary
+print("HRA:" , HRA)
+DA = (15 / 100) * basicsalary
+print("DA:" , DA)
+print("Total salary:" , basicsalary + HRA + DA)
