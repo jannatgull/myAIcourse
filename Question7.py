9a20a3eefc0549dfc5fac81cb341257bcdbb61c4
jannatgull <jannatgullpopalxee@gmail.com>
2025-08-19 15:23:46 +0500

    Ques.7
    Distribute candies equally

---
+++ Question7.py
@@ -0,0 +1,8 @@
+n = int(input("Enter number of Candies:"))
+k = int(input("Enter number of students:"))
+
+print("Number of candies each student gets:")
+print( n // k)
+
+print("Number of candies left:")
+print( n % k)
